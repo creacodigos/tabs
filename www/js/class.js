38386682abb1def32d7a80c47f71461f1a638c11
Jorge <jorge@creacodigos.com>
2020-05-28 18:22:53 +0100

    color por localStorage

--- www/js/class.js
+++ www/js/class.js
@@ -1,8 +1,25 @@
+function getColor(){
+
+    var color = localStorage.getItem('color');
+    var body = document.getElementsByTagName("body")[0];
+
+    if(color && color.length > 0)
+    {
+        body.classList.remove("oscuro");
+        body.classList.add(color);
+    }
+}
 function changeClass(valor){
     document.getElementsByTagName("body")[0].className = valor;
+    localStorage.setItem('color', valor);
 }
 function removerClass()
 {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("oscuro");
+    localStorage.removeItem('color');
 }
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    getColor();
+  });
